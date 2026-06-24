17acfc491b3d8adaf9274662a8083efd3773cb10
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-06-24 23:46:00 +0200

    feat: Added structural chunking for ppt files

--- DataIngestion/chunker.py
+++ DataIngestion/chunker.py
@@ -1,14 +1,28 @@
-from langchain_experimental.text_splitter import SemanticChunker
+from langchain_experimental.text_splitter import SemanticChunker as LangchainSemanticChunker
 from langchain_huggingface import HuggingFaceEmbeddings
+import re
 
-class PDFChunker:
+class SemanticChunker:
     def __init__(self,embedding_model):
         self.embedding_model = embedding_model
-        self.chunker=SemanticChunker(self.embedding_model,
+        self.chunker=LangchainSemanticChunker(self.embedding_model,
                                      breakpoint_threshold_type='percentile')
         
     
     def text_chunker(self,text):
         chunks=self.chunker.split_text(text)
+        return chunks
+    
+    
+    
+
+class StructuralChunker:
+    def __init__(self):
+        pass
+        
+    def text_chunker(self,text):
+        chunks=re.split(r'\[SLIDE \d+\]',text)
+        chunks = [chunk.strip() for chunk in chunks if chunk.strip()]
+        
         return chunks
     
