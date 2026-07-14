83d6d1139aa709fab4872c55e462095b6c928407
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-07-14 14:30:24 +0530

    feat: added quiz generation

--- services/llm.py
+++ services/llm.py
@@ -1,7 +1,8 @@
 import os
 from groq import AsyncGroq
-from prompts import SYSTEM_PROMPT
+from prompts import SYSTEM_PROMPT,get_quiz_prompt
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -10,6 +11,8 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 #get groq client
 groq_client=AsyncGroq(api_key=GROQ_API_KEY)
+
+
 
 
 async def get_answer(query,chunks):
@@ -28,3 +31,25 @@ async def get_answer(query,chunks):
     )
     
     return response.choices[0].message.content
+
+
+#the get quiz function takes the topic, format and number of questions and generates questions
+
+async  def get_quiz(topic,num_questions,chunks,format):
+    #get the context by joining chunks
+    context="\n".join(chunks)
+    
+    #get quiz prompt
+    quiz_prompt=get_quiz_prompt(format,num_questions)
+    
+    #these joined chunks go into llm
+    response=await groq_client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[{
+            "role":"system","content":quiz_prompt},
+            {"role":"user","content":f"Topic : {topic}\n\nContext: {context}"
+        }]
+    )
+    
+    results=response.choices[0].message.content
+    return json.loads(results)
