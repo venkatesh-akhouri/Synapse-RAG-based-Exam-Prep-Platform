e37b6b3d65993c6b4d633e166af252363e91a3e7
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-07-17 10:39:07 +0530

    refact: Added exception handling for different errors that can arise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,File,UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
 import tempfile
 import os
 from services.ingestion import ParserFactory,ChunkingFactory,EMBEDDINGS
@@ -10,8 +11,11 @@ from services.retreival import  retrieve
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from services.llm import get_answer,get_quiz
+from exception import UnsupportedFileError,DocumentParsingError,LLMGeneratorError
 
 load_dotenv()
+
+ACCEPTED_DOC_FORMATS=['pdf','doc','docx']
 
 
 #initialise groq api key
@@ -41,9 +45,15 @@ async def upload_files(files: list[UploadFile]):
             temp.write(await file.read())
             temp_path=temp.name
             
-        
-        parser=ParserFactory.call_parser(temp_path)
-        parsed_content=parser.parse()
+        try:
+            parser=ParserFactory.call_parser(temp_path)
+            parsed_content=parser.parse()
+        except UnsupportedFileError:
+            os.unlink(temp_path)
+            raise HTTPException(status_code=415, detail="Unsupported file type")
+        except DocumentParsingError:
+            os.unlink(temp_path)
+            raise HTTPException(status_code=422, detail="Document parsing error")
         #change the file name t original file
         parsed_content['file_name']=file.filename
         os.unlink(temp_path)
@@ -64,6 +74,7 @@ async def upload_files(files: list[UploadFile]):
 @app.post("/chat")
 async def chat(request: ChatRequest ):
     retreival_results=retrieve(request.message,embedder.collection)
+    
     print("----debugging-----")
     print(retreival_results["is_covered"])
     
@@ -73,9 +84,10 @@ async def chat(request: ChatRequest ):
         return ChatResponse(message="Topic not covered in uploaded materials",
                             is_covered=False)
     
-    
-    query_answer=await get_answer(request.message,retreival_results['chunks'])
-   
+    try:
+        query_answer=await get_answer(request.message,retreival_results['chunks'])
+    except LLMGeneratorError:
+        return ChatResponse(message="I'm sorry, I encountered an internal error while trying to generate your answer. Please try again.")
     return ChatResponse(
         message=query_answer,
         source=retreival_results["sources"][0],
@@ -89,16 +101,22 @@ async def chat(request: ChatRequest ):
 async def quiz(request: QuizRequest):
     retireval_results=retrieve(request.topic,embedder.collection)
     
+    
     #check if the content exists in the documents
     if not retireval_results['is_covered']:
         return QuizResponse(
             message="Topic not covered in uploaded materials",
             questions=[])
     
-    questions=await get_quiz(request.topic,
-                            request.num_questions,
-                             retireval_results['chunks'],
-                            request.format)
+    try:
+        questions=await get_quiz(request.topic,
+                                request.num_questions,
+                                 retireval_results['chunks'],
+                                request.format)
+    except LLMGeneratorError:
+        return QuizResponse(
+            message="Unable to generate quiz, please try again later!"
+        )
     
     
     
