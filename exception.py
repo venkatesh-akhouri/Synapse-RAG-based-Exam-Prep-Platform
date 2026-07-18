b4d2f2831133b7dbdf861a151e4b5fb3c0bcd3b8
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-07-18 09:27:28 +0530

    Base file for exception handling

---
+++ exception.py
@@ -0,0 +1,20 @@
+
+
+
+class SynapseException(Exception):
+    pass
+
+
+class UnsupportedFileError(SynapseException):
+    pass
+
+
+
+class LLMGeneratorError(SynapseException):
+    pass
+
+
+
+
+class DocumentParsingError(SynapseException):
+    pass
